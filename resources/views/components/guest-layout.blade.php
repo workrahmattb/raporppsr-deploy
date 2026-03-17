fd3f2957a043fddca8267d86f195b9531b0982e4
workrahmattb <workrahmattb@gmail.com>
2026-03-17 13:17:59 +0700

    perbaiki logo di tab

--- resources/views/components/guest-layout.blade.php
+++ resources/views/components/guest-layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
 
+    <link rel="icon" href="{{ asset('img/logo-ppsr.png') }}" type="image/png">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
