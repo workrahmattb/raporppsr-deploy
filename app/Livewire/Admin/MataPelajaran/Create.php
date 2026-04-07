dbdd688aabd737322b0ce61fb2a2f94eff6f15ec
workrahmattb <workrahmattb@gmail.com>
2026-04-07 11:42:40 +0700

    kalau ada code mata pelajaran yang sama fix

--- app/Livewire/Admin/MataPelajaran/Create.php
+++ app/Livewire/Admin/MataPelajaran/Create.php
@@ -14,21 +14,14 @@ class Create extends Component
     public $tingkat = '';
     public $kkm = 75;
 
-    protected function rules(): array
-    {
-        return [
-            'kode' => [
-                'required',
-                'string',
-                'max:10',
-            ],
-            'nama' => 'required|string|max:255',
-            'namapelajaran_arabic' => 'nullable|string|max:255',
-            'kelompok' => 'required|in:A,B,C',
-            'tingkat' => 'nullable|in:7,8,9,10,11,12',
-            'kkm' => 'required|integer|min:0|max:100',
-        ];
-    }
+    protected $rules = [
+        'kode' => 'required|string|max:10',
+        'nama' => 'required|string|max:255',
+        'namapelajaran_arabic' => 'nullable|string|max:255',
+        'kelompok' => 'required|in:A,B,C',
+        'tingkat' => 'nullable|in:7,8,9,10,11,12',
+        'kkm' => 'required|integer|min:0|max:100',
+    ];
 
     protected $messages = [
         'kode.required' => 'Kode mata pelajaran harus diisi',
